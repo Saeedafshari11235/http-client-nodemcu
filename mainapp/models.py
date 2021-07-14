6c923246b7867b4778fc0cdd85b275dfc176f61a
arha83 <arha113918@gmail.com>
2021-07-14 17:20:22 +0430

    Handled incoming pot value

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class INT(models.Model):
+	integer= models.IntegerField(default=None, null=True)
+	index= models.IntegerField(default=None, null=True)
+
+	def __str__(self):
+		return str(self.integer)
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import INT
 
 def homePage(request):
 
 	un=''
 	potV=0
+	print(dict(request.GET))
 
 	if 'username' in dict(request.GET).keys():
-		if dict(request.GET)['username'] == 'nodemcu': un= 'nodemcu' #username --> nodemcu
+		if dict(request.GET)['username'][0] == 'nodemcu': un= 'nodemcu' #username --> nodemcu
 	if un == 'nodemcu':
-		if 'pot' in dict(request.GET).keys(): potV= dict(request.GET)['pot']
-	print(potV)
+		if 'pot' in dict(request.GET).keys():
+			potV= int(dict(request.GET)['pot'][0])
+			if len(INT.objects.all()) == 0:
+				x= INT(integer=potV, index=0)
+				x.save()
+			else:
+				y=INT.objects.filter(index=0)
+				y.delete()
+				x= INT(integer=potV, index=0)
+				x.save()
+	else:
+		return render(request, 'home.html', {'pot': INT.objects.get(index=0)})
+
+	return HttpResponse('bonjour')
+
+	'''print(potV)
+	return render(request, 'home.html', {'pot': potV})'''
 
 
