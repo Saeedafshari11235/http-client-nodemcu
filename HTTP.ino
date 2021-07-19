48bd246f3777c43f412da045d8e979ace4839169
Saeedafshari11235 <86922433+Saeedafshari11235@users.noreply.github.com>
2021-07-19 11:42:02 +0430

    change the server

--- HTTP.ino
+++ HTTP.ino
@@ -1,54 +1,65 @@
+//it's a program that connect to a server and share information using http
+//add libraries
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
-
+//set the connection information
 const char* ssid = "Afshari-tcz";
 const char* password = "Saeed@#Hesam";
+//set a pin to get the value of pot
 const int analogInPin = A0;
-
+//value of pot
 int sensorValue = 0;
-String serverName = "http://192.168.1.50:8000";
+//name of server and the port
+String serverName = "http://arha83.pythonanywhere.com/:443";
+//i don't why it is here but code works
 unsigned long lastTime = 0;
-unsigned long timerDelay = 200;
-int pot;
+//delay of sending info
+unsigned long timerDelay = 3000;
 
 void setup() {
+  //set the speed
   Serial.begin(115200);
-
+  //connecting to the wifi
   WiFi.begin(ssid, password);
   Serial.println("Connecting");
+  //while the device is not connected...
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
+  //print the wifi ip
   Serial.println(WiFi.localIP());
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
   delay(2000);
   digitalWrite(LED_BUILTIN, LOW);
-  digitalWrite(LED_BUILTIN, LOW);
-  digitalWrite(LED_BUILTIN, LOW);
-  digitalWrite(LED_BUILTIN, LOW);
-
-  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
 }
 
 
 void loop() {
+  //get the value of sensor from pin
   sensorValue = analogRead(analogInPin);
 
   if ((millis() - lastTime) > timerDelay) {
-
+    //if the wifi is still connected
     if (WiFi.status() == WL_CONNECTED) {
+      //Creates a client that can connect to to a specified internet IP address and port as defined in client.connect().--->Arduino.cc
       WiFiClient client;
+      //make a var as httpclient 
       HTTPClient http;
+      //make a web address using GET method and put it in a string var
+      //for GET method use "/?" at first and use "&" to separate information
       String serverPath = serverName + "/?username=nodemcu" + "&" + "pot=" + String(sensorValue);
       Serial.print("\npotValue : ");
       Serial.println(sensorValue);
       delay(500);
+      //begins connecting to server
+      //i don't know what c_str()is
       http.begin(client, serverPath.c_str());
+      //get the response code using GET
       int httpResponseCode = http.GET();
 
       if (httpResponseCode > 0) {
